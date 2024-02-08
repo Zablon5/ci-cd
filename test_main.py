2260b8b19da8598aa15244e951957c4b50a2929e
Zablon5 <zgirma74@gmail.com>
2024-02-08 18:54:07 +0300

    add a workflow

---
+++ test_main.py
@@ -0,0 +1,8 @@
+import pytest
+from main import fizzbuzz
+
+def test_fizzbuzz():
+    assert fizzbuzz(3) == 'Fizz'
+    assert fizzbuzz(5) == 'Buzz'
+    assert fizzbuzz(15) == 'FizzBuzz'
+    assert fizzbuzz(2) == '2'
